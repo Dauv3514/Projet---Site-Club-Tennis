eb61176adb78facd07691a3e8dfe65fcdc4974b5
Valentin Dauvier <valentindauvier@hotmail.fr>
2022-03-28 16:46:45 +0200

    modification site

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var i = 0;
-var images = ['images/Sans titre2.png', 'images/Sans titre.png', 'images/Sans titre3.png'];
+var images = ['images/Sans titre2.png', 'images/Sans titre.png'];
 var time = 5000;
 
 function changeImg() {
@@ -34,32 +34,38 @@ window.addEventListener('scroll', () => {
 
 
 
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
 
+// Images are from unsplash
+let pictures = ['jourdec1.jpg','jourdec2.jpg','jourdec3.jpg','jourdec4.jpg'];
 
+img.src = pictures[0];
+let position = 0;
 
-const imgs = document.getElementById('java');
-const nbSlide = imgs.length;
-const suivant = document.querySelector('.right');
-const precedent = document.querySelector('.left');
-let count = 0;
-
-function slideSuivante() {
-
-    imgs[count].classList.remove('active');
-
-    if (count < nbSlide - 1) {
-        count++;
-    } else {
-        count = 0;
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
     }
-
-    imgs[count].classList.add('active')
-    console.log(count);
+    img.src = pictures[position + 1];
+    position++;
 }
 
-suivant.addEventListener('click', slideSuivante)
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
 
-
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
 
 
 
